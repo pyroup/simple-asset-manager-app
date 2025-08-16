6223c987f4693f22a81475c56f7406ff871e3d09
Goshi Shimotani <pyroup4869@gmail.com>
2025-08-16 10:33:40 +0900

    Freeプランにデプロイできるよう修正

--- hands-on-final-version/app.py
+++ hands-on-final-version/app.py
@@ -35,5 +35,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=config.DEBUG) 
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, threaded=True, use_reloader=False) 
